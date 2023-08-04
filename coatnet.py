f4138f0232ba5abc4af0ae349353ddbcb83f3670
adminuser <adminuser@kali.mvm>
2023-08-04 19:50:02 +0530

    added mlp layer

--- coatnet.py
+++ coatnet.py
@@ -60,3 +60,60 @@ class SE(tf.keras.layers.Layer):
         print(y.shape, x.shape)
 
         return x * y
+
+
+class MLP(tf.keras.layers.Layer):
+    """MLP as used in Vision Transformer, MLP-Mixer and related networks"""
+
+    def __init__(
+        self,
+        hidden_dim: int,
+        projection_dim: int,
+        drop_rate: float,
+        act_layer: str,
+        kernel_initializer: str = "glorot_uniform",
+        bias_initializer: str = "zeros",
+        **kwargs,
+    ):
+        super(MLP, self).__init__(**kwargs)
+        self.hidden_dim = hidden_dim 
+        self.projection_dim = projection_dim 
+        self.drop_rate = drop_rate
+        self.kernel_initializer = kernel_initializer
+        self.bias_initializer = bias_initializer
+
+        act_layer = act_layer_factory(act_layer)
+
+        self.fc1 = tf.keras.layers.Dense(
+            units=hidden_dim,
+            kernel_initializer=kernel_initializer,
+            bias_initializer=bias_initializer,
+            name="fc1",
+        )
+        self.act = act_layer()
+        self.drop1 = tf.keras.layers.Dropout(rate=drop_rate)
+        self.fc2 = tf.keras.layers.Dense(
+            units=projection_dim,
+            kernel_initializer=kernel_initializer,
+            bias_initializer=bias_initializer,
+            name="fc2",
+        )
+        self.drop2 = tf.keras.layers.Dropout(rate=drop_rate)
+
+    def call(self, x, training=False):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x, training=training)
+        x = self.fc2(x)
+        x = self.drop2(x, training=training)
+        return x
+
+    def get_config(self):
+        config = super(MLP, self).get_config()
+        config["hidden_dim"] = self.hidden_dim
+        config["projection_dim"] = self.projection_dim
+        config["drop_rate"] = self.drop_rate
+        config["kernel_initializer"] = self.kernel_initializer
+        config["bias_initializer"] = self.bias_initializer
+        return config
+                              
